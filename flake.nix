4b5ab1c534d51f29ae3801f317b084acc4454ced
winston <hey@winston.sh>
2023-07-09 20:41:18 +0200

    build: begone, flake-utils

--- flake.nix
+++ flake.nix
@@ -1,35 +1,39 @@
 {
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        packages = rec {
-          icat = pkgs.writeShellApplication {
-            name = "icat";
-            runtimeInputs = with pkgs; [
-              coreutils
-              curl
-              ffmpeg
-              file # to identify mime types
-              gnugrep
-              imagemagick
-              perl # for shasum
-            ];
-            text = builtins.readFile ./icat.sh;
-          };
-          default = icat;
-        };
-        apps = rec {
-          icat = flake-utils.lib.mkApp {drv = self.packages.${system}.icat;};
-          default = icat;
-        };
-      }
-    );
+  outputs = {nixpkgs, ...} @ inputs: let
+    systems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "armv6l-linux"
+      "armv7l-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      icat = pkgs.writeShellApplication {
+        name = "icat";
+        runtimeInputs = with pkgs; [
+          coreutils
+          curl
+          ffmpeg
+          file # to identify mime types
+          gnugrep
+          imagemagick
+          perl # for shasum
+        ];
+        text = builtins.readFile ./icat.sh;
+      };
+      default = icat;
+    });
+    apps = forAllSystems (system: rec {
+      icat = {
+        type = "app";
+        program = "${inputs.self.packages.${system}.icat}/bin/icat";
+      };
+      default = icat;
+    });
+  };
 }
